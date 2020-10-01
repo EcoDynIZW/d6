0aa0f32cf090e8e4491f96c8282a755f9322038d
Cédric Scherer <cedricphilippscherer@gmail.com>
2020-10-01 12:59:49 +0200

    :heavy_plus_sign: add submission script

---
+++ inst/XX_submit.R
@@ -0,0 +1,7 @@
+## Render your scripts to html
+## Render all .Rmd files placed in ./R
+d6::render_all_reports()
+## Or render each step by step
+## Add additonal lines for each .Rmd file placed in ./R that you want to render
+d6::render_report("myRMD.Rmd")
+ 
