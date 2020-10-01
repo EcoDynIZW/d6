1a1a4ab96da56527f5a1e6e63b53aaede6dc5ba9
Cédric Scherer <cedricphilippscherer@gmail.com>
2020-10-01 13:01:26 +0200

    :sparkles: add render_all_reports() and render_report()

---
+++ R/render_report.R
@@ -0,0 +1,32 @@
+#' Render Rmd placed in ./R to html in ./docs/reports
+#'
+#' @param rmd A string. Name of the Rmd file
+#' @importFrom here here
+#' @importFrom rmarkdown render
+#' 
+render_report <- function(rmd) {
+  rmarkdown::render(
+    paste0("R/", rmd), 
+    output_file = here::here(
+      "docs", "reports", 
+      paste0(gsub(".Rmd|.rmd|.RMD", x = rmd, replacement = ""), ".html")
+    )
+  )
+}
+
+render_all_reports <- function() {
+  rmds <- list.files(here::here("R"), pattern = "*.rmd|*.Rmd|*.RMD", recursive = TRUE, include.dirs = TRUE)
+  out <- gsub(".*/","", rmds)
+  
+  purrr::map2(
+    rmds,
+    out,
+    ~rmarkdown::render(
+      paste0("R/", .x), 
+      output_file = here::here(
+        "docs", "reports", 
+        paste0(gsub(".Rmd|.rmd|.RMD", x = .y, replacement = ""), ".html")
+      )
+    )
+  )
+}
