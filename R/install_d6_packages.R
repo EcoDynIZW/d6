25fd4dd026bb55d0b0cdac327ecde91c0458301e
Cédric Scherer <cedricphilippscherer@gmail.com>
2020-10-01 13:01:15 +0200

    :sparkles: add install_d6_packages()

---
+++ R/install_d6_packages.R
@@ -0,0 +1,23 @@
+#' Install common packages used in the D6 department
+#'
+#' @param geo Boolean. Do you intent to process geodata? Installs important geo packages if `TRUE` (the default)
+#'
+
+install_d6_packages <- function(geo = TRUE) {
+  list.of.packages <- c(
+    "tidyverse", 
+    "lme4",
+    "glmmTMB"
+  )
+  if(geo == TRUE) {
+    list.of.packages <- c(
+      list.of.packages,
+      "rgdal",
+      "rgeos",
+      "raster",
+      "sf"
+    )
+  }
+  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+}
