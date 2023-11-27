08561d78bdd45ffbf267d231e8212570600d9ca7
z3tt <cedricphilippscherer@gmail.com>
2023-11-27 11:42:30 +0100

    restructure install logic in simple_load()

--- R/simple_load.R
+++ R/simple_load.R
@@ -15,28 +15,31 @@ simple_load <- function(pckgs = NULL, update_cran = FALSE, update_gh = FALSE) {
   pckgs_gh   <- grep("/", pckgs, value = TRUE)
   pckgs_cran <- pckgs[!(pckgs %in% pckgs_gh)]
   
-  ## install cran packages if needed
-  ## use all CRAN packages if update_cran is TRUE and a subset otherwise
+  
+  ## install all listed CRAN packages if update_cran is TRUE, 
+  ## otherwise only those that are not yet installed
   if (update_cran) { 
-    inst_cran <- pckgs_cran 
+    install.packages(pckgs_cran) 
   } else {
     inst_cran <- pckgs_cran %in% installed.packages() 
+    if(length(pckgs_cran[!inst_cran]) > 0) install.packages(pckgs_cran[!inst_cran])
   }
-  if(length(pckgs_cran[!inst_cran]) > 0) install.packages(pckgs_cran[!inst_cran])
   
-  ## install github package if needed
-  ## use all GitHub packages if update_gh is TRUE and a subset otherwise
-  if (update_gh == FALSE) { 
-    inst_gh <- pckgs_gh 
+  
+  ## install all listed GitHub packages if update_gh is TRUE, 
+  ## otherwise only those that are not yet installed
+  ## if any GitHub packages need to be installed, remotes is installed if needed
+  if (update_gh) { 
+    if (!("remotes" %in% installed.packages())) install.packages("remotes")
+    remotes::install_github(pckgs_gh, force = TRUE) 
   } else {
     inst_gh <- basename(pckgs_gh) %in% installed.packages()
-  }
-  if (length(pckgs_gh[!inst_gh]) > 0) {
-    ## remotes needed to install github packages
-    if (!("remotes" %in% installed.packages())) install.packages("remotes")
-    remotes::install_github(pckgs_gh[!inst_gh], force = TRUE)
+    if (length(pckgs_gh[!inst_gh]) > 0) {
+      if (!("remotes" %in% installed.packages())) install.packages("remotes")
+      remotes::install_github(pckgs_gh[!inst_gh])
+    }
   }
   
-  ## load packages
+  ## load packages (basename() is used to keep only the package name from GitHub inputs)
   invisible(sapply(basename(pckgs), library, character.only = TRUE, logical.return = TRUE))
 }
