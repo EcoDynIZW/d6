5ea830a8989e3aef76e3ce188d5ad17fb8158514
z3tt <cedricphilippscherer@gmail.com>
2023-11-27 11:20:04 +0100

    add arguments to update packages via `simple_load()`

--- R/simple_load.R
+++ R/simple_load.R
@@ -2,27 +2,39 @@
 #'
 #' @param pckgs A vector of packages to load. Packages from CRAN and GitHub can 
 #' be passed simultaneously.
+#' @param update_cran Logical. Should CRAN packages be updated? Defaults to FALSE.
+#' @param update_gh Logical. Should GitHub packages be updated? Defaults to FALSE.
 #' 
 #' @export
 #' 
 #' @examples simple_load("ggplot2")
 #' 
-simple_load <- function(pckgs = NULL) {
+simple_load <- function(pckgs = NULL, update_cran = FALSE, update_gh = FALSE) {
 
   ## split packages into cran and github packages
   pckgs_gh   <- grep("/", pckgs, value = TRUE)
   pckgs_cran <- pckgs[!(pckgs %in% pckgs_gh)]
   
   ## install cran packages if needed
-  inst_cran <- pckgs_cran %in% installed.packages()
+  ## use all CRAN packages if update_cran is TRUE and a subset otherwise
+  if (update_cran) { 
+    inst_cran <- pckgs_cran 
+  } else {
+    inst_cran <- pckgs_cran %in% installed.packages() 
+  }
   if(length(pckgs_cran[!inst_cran]) > 0) install.packages(pckgs_cran[!inst_cran])
   
   ## install github package if needed
-  inst_gh <- basename(pckgs_gh) %in% installed.packages()
+  ## use all GitHub packages if update_gh is TRUE and a subset otherwise
+  if (update_gh == FALSE) { 
+    inst_gh <- pckgs_gh 
+  } else {
+    inst_gh <- basename(pckgs_gh) %in% installed.packages()
+  }
   if (length(pckgs_gh[!inst_gh]) > 0) {
     ## remotes needed to install github packages
     if (!("remotes" %in% installed.packages())) install.packages("remotes")
-    remotes::install_github(pckgs_gh[!inst_gh])
+    remotes::install_github(pckgs_gh[!inst_gh], force = TRUE)
   }
   
   ## load packages
